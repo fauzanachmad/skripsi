91e68dcb14c0034e5ce619ff560bf79b7892de00
ojanachmad321@gmail.com <102979420+achmadfauzan22@users.noreply.github.com>
2022-08-11 09:27:30 -0700

    roy

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Support\Facades\Http;
+
 class HomeController
 {
     public function index()
     {
-        return view('home');
+        $logger = $this->logger();
+
+        return view('home', compact('logger'));
+    }
+
+    public function logger() {
+        $clasification = Http::get('http://localhost:5000/report/clasification');
+        $conf_matrix = Http::get('http://localhost:5000/report/confusion-matrix');
+
+        return [json_decode($clasification->json(), true), json_decode($conf_matrix->json(), true)];
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                     <h4 class="text-center">Precision</h4>
                 </div>
                 <div class="card-body">
-                    <p class="text-center">0,74</p>
+                    <p class="text-center">0,76</p>
                 </div>
             </div>
         </div>
@@ -18,7 +18,7 @@
                     <h4 class="text-center">Recall</h4>
                 </div>
                 <div class="card-body">
-                    <p class="text-center">0,74</p>
+                    <p class="text-center">0,76</p>
                 </div>
             </div>
         </div>
@@ -28,7 +28,7 @@
                     <h4 class="text-center">Accuracy</h4>
                 </div>
                 <div class="card-body">
-                    <p class="text-center">0,74</p>
+                    <p class="text-center">0,76</p>
                 </div>
             </div>
         </div>
@@ -38,26 +38,72 @@
                     <h4 class="text-center">F1Score</h4>
                 </div>
                 <div class="card-body">
-                    <p class="text-center">0,74</p>
+                    <p class="text-center">0,76</p>
                 </div>
             </div>
         </div>
     </div>
-    {{-- <div class="row">
-        <div class="col-lg-12">
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    Dashboard
+                    Clasification Report
                 </div>
-
+            
                 <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in! --}}
+                    <table class="table table-bordered table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <td>#</td>
+                            <td>Precission</td>
+                            <td>Recall</td>
+                            <td>F1-Score</td>
+                            <td>Support</td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Kurang Populer</td>
+                            <td>{{ $logger[0]['classification_report']['kurang_populer']['precision'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['kurang_populer']['recall'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['kurang_populer']['f1-score'] ?? '' }}</td>
+                            <td>{{ $logger[0]['classification_report']['kurang_populer']['support'] ?? ''}}</td>
+                        </tr>
+                        <tr>
+                            <td>Populer</td>
+                            <td>{{ $logger[0]['classification_report']['populer']['precision'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['populer']['recall'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['populer']['f1-score'] ?? '' }}</td>
+                            <td>{{ $logger[0]['classification_report']['populer']['support'] ?? ''}}</td>
+                        </tr>
+                        <tr>
+                            <td>Standar</td>
+                            <td>{{ $logger[0]['classification_report']['standar']['precision'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['standar']['recall'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['standar']['f1-score'] ?? '' }}</td>
+                            <td>{{ $logger[0]['classification_report']['standar']['support'] ?? ''}}</td>
+                        </tr>
+                        <tr>
+                            <td>Macro Averrage</td>
+                            <td>{{ $logger[0]['classification_report']['macro avg']['precision'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['macro avg']['recall'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['macro avg']['f1-score'] ?? '' }}</td>
+                            <td>{{ $logger[0]['classification_report']['macro avg']['support'] ?? ''}}</td>
+                        </tr>
+                        <tr>
+                            <td>Weighted Averrage</td>
+                            <td>{{ $logger[0]['classification_report']['weighted avg']['precision'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['weighted avg']['recall'] ?? ''}}</td>
+                            <td>{{ $logger[0]['classification_report']['weighted avg']['f1-score'] ?? '' }}</td>
+                            <td>{{ $logger[0]['classification_report']['weighted avg']['support'] ?? ''}}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="4">Accuracy</td>
+                            <td>{{ $logger[0]['classification_report']['accuracy'] ?? ''}}</td>
+                        </tr>
+                        
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -66,5 +112,4 @@
 @endsection
 @section('scripts')
 @parent
-
 @endsection
